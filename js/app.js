4f8bb913ec5e6aad48034ba3152500e7682c1932
johncastiblanco <johncastiblanco@puntoexe.com.co>
2016-11-23 18:53:55 -0500

    se integra Angular y su controlador de entorno

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var menu = angular.module("MenuPrincipal", []);
+
+menu.controller("EntornoController", function($scope) { 
+
+	$scope.nombre= "Mario";
+	$scope.apellidos= "Flores";
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
